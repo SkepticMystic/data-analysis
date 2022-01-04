96dcd951cb9dda459ae6062cc4872dc54e828421
Ross Keenan <rossk29@gmail.com>
2022-01-04 13:05:22 +0200

    refactor(ChartModal): :recycle: Better null checking & naming

--- src/Components/Analysis.svelte
+++ src/Components/Analysis.svelte
@@ -11,10 +11,15 @@
 	const { index, settings } = plugin;
 	const { fieldsToCheck } = settings;
 
-	let options = fieldsToCheck;
+	let allFields = fieldsToCheck;
 	let selected: string[] = [];
 
 	let colour = "#15a252";
+
+	function isValidSelection(selected: string[]) {
+		return selected.length === 2;
+	}
+
 	interface Datum2d {
 		x: number;
 		y: number;
@@ -24,20 +29,22 @@
 		return index
 			.map((page) => {
 				return {
-					x: (page[selected[0]]?.[0] ?? 0) as number,
-					y: (page[selected[1]]?.[0] ?? 0) as number,
+					x: page[selected[0]]?.[0] as number,
+					y: page[selected[1]]?.[0] as number,
 				};
 			})
 			.filter((point) => {
-				return point.x !== 0 && point.y !== 0;
+				return point.x && point.y;
 			});
 	}
 
-	function refreshCorrelation(innerData: Datum2d[]) {
-		return pearsonCorrelation(
-			innerData.map((p) => p.x),
-			innerData.map((p) => p.y)
-		);
+	function refreshCorrelation(selected: string[], innerData: Datum2d[]) {
+		return isValidSelection(selected)
+			? pearsonCorrelation(
+					innerData.map((p) => p.x),
+					innerData.map((p) => p.y)
+			  )
+			: null;
 	}
 
 	function refreshData(colour: string, innerData: Datum2d[]) {
@@ -107,6 +114,7 @@
 	};
 </script>
 
+<Checkboxes options={allFields} {plugin} bind:selected />
 <ChartOptions bind:colour />
 <Scatter {data} options={chartOptions} />
-<div>Correlation: {correlation}</div>
+<div>Correlation: {correlation?.toFixed(4) ?? "Select 2 fields"}</div>
