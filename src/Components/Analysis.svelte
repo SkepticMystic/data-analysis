825c5f773f2e51bdf6fd5167bd2f7d7f03648fdb
Ross Keenan <rossk29@gmail.com>
2022-01-04 11:26:32 +0200

    feat(ChartModal): :sparkles: Working Scatterplot!

--- src/Components/Analysis.svelte
+++ src/Components/Analysis.svelte
@@ -1,9 +1,126 @@
 <script lang="ts">
 	import { AnalysisModal } from "src/AnalysisModal";
+	import Checkboxes from "./Checkboxes.svelte";
+	import Scatter from "svelte-chartjs/src/Scatter.svelte";
+	import { pearsonCorrelation } from "src/analyses";
 
 	export let modal: AnalysisModal;
 
 	const { app, plugin } = modal;
+	const { index, settings } = plugin;
+	const { fieldsToCheck } = settings;
+
+	let options = fieldsToCheck;
+	let selected: string[] = [];
+
+	interface Datum2d {
+		x: number;
+		y: number;
+	}
+
+	function refreshInnerData(selected: string[]): Datum2d[] {
+		return index
+			.map((page) => {
+				return {
+					x: (page[selected[0]]?.[0] ?? 0) as number,
+					y: (page[selected[1]]?.[0] ?? 0) as number,
+				};
+			})
+			.filter((point) => {
+				return point.x !== 0 && point.y !== 0;
+			});
+	}
+
+	function refreshCorrelation(innerData: Datum2d[]) {
+		return pearsonCorrelation(
+			innerData.map((p) => p.x),
+			innerData.map((p) => p.y)
+		);
+	}
+
+	let innerData = refreshInnerData(selected);
+	let correlation = refreshCorrelation(innerData);
+
+	$: {
+		innerData = refreshInnerData(selected);
+		correlation = refreshCorrelation(innerData);
+		console.log(innerData);
+	}
+
+	let data = {
+		labels: ["Scatter"],
+		datasets: [
+			{
+				borderColor: "rgba(99,0,125, .2)",
+				backgroundColor: "rgba(99,0,125, .5)",
+				label: "V(node2)",
+				data: innerData,
+			},
+		],
+	};
+
+	$: data = {
+		labels: ["Scatter"],
+		datasets: [
+			{
+				borderColor: "rgba(99,0,125, .2)",
+				backgroundColor: "red",
+				label: "V(node2)",
+				data: innerData,
+			},
+		],
+	};
+
+	let chartOptions = {
+		title: {
+			display: true,
+			text: "Scatter Chart - Logarithmic X-Axis",
+		},
+		scales: {
+			xAxes: [
+				{
+					label: "Test",
+					type: "linear",
+					position: "bottom",
+					ticks: {
+						userCallback: function (tick) {
+							var remain =
+								tick /
+								Math.pow(
+									10,
+									Math.floor(Chart.helpers.log10(tick))
+								);
+							if (remain === 1 || remain === 2 || remain === 5) {
+								return tick.toString() + "Hz";
+							}
+							return "";
+						},
+					},
+					scaleLabel: {
+						labelString: "Frequency",
+						display: true,
+					},
+				},
+			],
+			yAxes: [
+				{
+					type: "linear",
+					ticks: {
+						userCallback: function (tick) {
+							return tick.toString() + "dB";
+						},
+					},
+					scaleLabel: {
+						labelString: "Voltage",
+						display: true,
+					},
+				},
+			],
+		},
+	};
 </script>
 
 <Checkboxes {options} {plugin} bind:selected />
+
+<Scatter {data} options={chartOptions} />
+<div>Correlation: {correlation}</div>
