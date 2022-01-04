9f8c3618de2a76ac976d73a439d3b909727b557c
Ross Keenan <rossk29@gmail.com>
2022-01-04 10:40:54 +0200

    refactor: :recycle: Add Dataview api

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Plugin } from "obsidian";
+import { DataviewApi, Link } from "obsidian-dataview";
 import { DEFAULT_SETTINGS } from "./const";
 import { Settings } from "./interfaces";
 import { SettingTab } from "./SettingTab";
@@ -8,11 +8,46 @@ export default class DataAnalysisPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
+
 		this.addSettingTab(new SettingTab(this.app, this));
+
+		if (this.app.plugins.enabledPlugins.has("dataview")) {
+			console.log("DataView plugin is enabled");
+			const api = this.app.plugins.plugins.dataview?.api;
+			if (api) {
+				console.log(this.refreshIndex(api));
+			} else {
+				this.registerEvent(
+					this.app.metadataCache.on("dataview:api-ready", (api) => {
+						console.log(this.refreshIndex(api));
+					})
+				);
+			}
+		} else {
+			new Notice(
+				"Dataview must be enabled for the Data Analysis plugin to work"
+			);
+		}
 	}
 
 	onunload() {}
 
+	unproxy(item: any): (number | string | Date | Link)[] {
+		const unproxied = [];
+
+		const queue = [item];
+		while (queue.length) {
+			const currItem = queue.shift();
+			// "Proxy" for checking if `currItem` is a proxy
+			if (typeof currItem.defaultComparator === "function") {
+				const possibleUnproxied = Object.assign({}, currItem);
+				const { values } = possibleUnproxied;
+				if (values) queue.push(...values);
+				else unproxied.push(possibleUnproxied);
+			} else unproxied.push(currItem);
+		}
+		return unproxied;
+	}
 	async loadSettings() {
 		this.settings = Object.assign(
 			{},
