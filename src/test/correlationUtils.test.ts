dcb717028d8cbb6c16acfeaff9bd7fb2df43a011
Ross Keenan <rossk29@gmail.com>
2022-01-09 08:56:18 +0200

    test: :white_check_mark: Successfully run test locally

--- src/test/correlationUtils.test.ts
+++ src/test/correlationUtils.test.ts
@@ -1,18 +1,48 @@
-import {buildAllPairs} from "../correlationUtils";
+import { buildAllPairs } from "../correlationUtils";
 
 function factorial(num: number): number {
-    if (num < 0) 
-            return -1;
-    else if (num == 0) 
-        return 1;
-    else {
-        return (num * factorial(num - 1));
-    }
+	if (num < 0) return -1;
+	else if (num == 0) return 1;
+	else {
+		return num * factorial(num - 1);
+	}
 }
 
-test('buildAllPairs creates correlation pairs without missing any fields and without duplicates', () => {
-    const input = ["apple", "banana", "strawberries", "pears", "grapefruit", "bread", "milk"]
-    const results = buildAllPairs(input)
-    expect(results.length).toEqual(factorial(input.length)/(2*factorial(input.length-2)));
-    expect(results).toEqual([["apple", "banana",], ["apple", "strawberries",], ["apple", "pears",], ["apple", "grapefruit",], ["apple", "bread",], ["apple", "milk",], ["banana", "strawberries",], ["banana", "pears",], ["banana", "grapefruit",], ["banana", "bread",], ["banana", "milk",], ["strawberries", "pears",], ["strawberries", "grapefruit",], ["strawberries", "bread",], ["strawberries", "milk",], ["pears", "grapefruit",], ["pears", "bread",], ["pears", "milk",], ["grapefruit", "bread",], ["grapefruit", "milk",], ["bread", "milk",],])
-})
+test("buildAllPairs creates correlation pairs without missing any fields and without duplicates", () => {
+	const input = [
+		"apple",
+		"banana",
+		"strawberries",
+		"pears",
+		"grapefruit",
+		"bread",
+		"milk",
+	];
+	const results = buildAllPairs(input);
+	expect(results.length).toEqual(
+		factorial(input.length) / (2 * factorial(input.length - 2))
+	);
+	expect(results).toEqual([
+		["apple", "banana"],
+		["apple", "strawberries"],
+		["apple", "pears"],
+		["apple", "grapefruit"],
+		["apple", "bread"],
+		["apple", "milk"],
+		["banana", "strawberries"],
+		["banana", "pears"],
+		["banana", "grapefruit"],
+		["banana", "bread"],
+		["banana", "milk"],
+		["strawberries", "pears"],
+		["strawberries", "grapefruit"],
+		["strawberries", "bread"],
+		["strawberries", "milk"],
+		["pears", "grapefruit"],
+		["pears", "bread"],
+		["pears", "milk"],
+		["grapefruit", "bread"],
+		["grapefruit", "milk"],
+		["bread", "milk"],
+	]);
+});
